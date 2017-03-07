6f653f56a452e160f9a4b4362b661b57c7d44d06
user <user@es302n36>
2017-03-07 16:12:29 +0800

    the progress of abstraction

--- src/oo/abstraction/Car.java
+++ src/oo/abstraction/Car.java
@@ -4,6 +4,14 @@ public class Car {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		 String brand;
+		    int cc;
+		    String name;
+		    String type;
+		
+
+		
 
 	}
 
