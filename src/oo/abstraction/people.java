ec986f3fb263bebf4bec8fe7b8fbfb687c99c402
user <user@es302n09>
2017-03-15 16:54:53 +0800

    homework

--- src/oo/abstraction/people.java
+++ src/oo/abstraction/people.java
@@ -2,26 +2,24 @@ package oo.abstraction;
 
 public class people {
 
+	int numbers;
+	int women;
+	int men;
+	int children;
+	 public people(int women,int men,int children ,int numbers ){
+		  this.numbers=numbers;
+		  this.numbers=numbers;
+			
+			
+			
+			
+			
+			
+			
+		}
 	public static void main(String[] args) {
 	    
-
-		int numbers;
-		String women;
-		String men;
-		String children;
+	
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	}
 
 }
