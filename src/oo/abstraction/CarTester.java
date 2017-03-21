4b2bf3676c6070632f6138d29509f4bd89d4de0b
user <user@es302n36>
2017-03-21 16:26:25 +0800

    Array and Class

--- src/oo/abstraction/CarTester.java
+++ src/oo/abstraction/CarTester.java
@@ -3,15 +3,36 @@ package oo.abstraction;
 public class CarTester {
 
 	public static void main(String[] args) {
-		Car c2 = new Car( "jjj","ggg",123123);
+		Car c2 = new Car( "uuu","ggg",235465462);
+		Car c3 = new Car( "yuyuy","ggg",232546453);
+		Car c4 = new Car( "tuytur","ggg",232378686);
+		Car[] cars =new Car[3];
+		cars [0] = new Car( "uuu","ggg",235465462);
+		cars [1] =new Car( "yuyuy","ggg",232546453);
+		cars [2] = new Car( "tuytur","ggg",232378686);
+		for(int ii =0;ii<3;ii++){
+		System.out.println(cars[ii].name);
+		System.out.println(cars[ii].cc);
+		System.out.println(cars[ii].type);
 		
-		
-		
-		
-		
-		
-		
+		}
+		class jjdfgfhjkhjkg{
+			
+			
+			
+			
+		}
 
 	}
 
+	class JJdfgfg{
+		
+		
+		
+	}
+	JJdfgfg  hgh= new JJdfgfg();
+	
+	
+	
+	
 }
