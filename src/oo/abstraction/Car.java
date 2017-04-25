e6e87320df0b06b2c60b72fa3aadf2bb3933804f
user <user@es302n02>
2017-04-25 16:24:02 +0800

    425

--- src/oo/abstraction/Car.java
+++ src/oo/abstraction/Car.java
@@ -1,11 +1,5 @@
 package oo.abstraction;
-
-
-
-
-
 public class Car {
-
         String brand;
 	    int cc;
 	    String name;
@@ -22,29 +16,17 @@ public class Car {
 	 public Car(String type,String name,int cc){
 		    this.cc=cc;
 		    this. name=name;
-		    this. type=type;
-		   
-		 
+		    this. type=type; 
 	 }
 		    public  void  setMilage(int m){
 		    	milage=m;
-		    	
-		    	
-		    	
-		  
 		    }
 		    public  float  addMilage(int m){
 		    	milage=m;
 		    	return milage;
-		    	
-		  
 		    }
-
 		    public  void maintain(int s){
 		    	status=s;
-		    
-	
-		  
 		    }
 
 		 
